afd6ec55f8535b28df8c4a9914c28ee465b43f76
BSEF19M517 <bsef19m517@pucit.edu.pk>
2022-04-26 11:47:05 +0500

    done lecture 5

--- app/src/main/java/com/bsef19m517/mc01/MainActivity.java
+++ app/src/main/java/com/bsef19m517/mc01/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_linear_horizontal1);
+        setContentView(R.layout.activity_main);
         etName = (EditText) findViewById(R.id.editTextTextPersonName);
         etPass = (EditText) findViewById(R.id.editTextTextPersonName2);
     }
