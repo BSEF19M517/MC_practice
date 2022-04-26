0e34e83d93539294ac7d6a4f72f59719ee9d5f77
BSEF19M517 <bsef19m517@pucit.edu.pk>
2022-04-26 10:47:57 +0500

    linear horizontal

--- app/src/main/java/com/bsef19m517/mc01/MainActivity.java
+++ app/src/main/java/com/bsef19m517/mc01/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_linear1);
+        setContentView(R.layout.activity_linear_horizontal1);
         etName = (EditText) findViewById(R.id.editTextTextPersonName);
         etPass = (EditText) findViewById(R.id.editTextTextPersonName2);
     }
