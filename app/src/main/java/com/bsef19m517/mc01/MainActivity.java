3d699c8c3523b1eaf4d5a8aee87c30db513bb2cd
BSEF19M517 <bsef19m517@pucit.edu.pk>
2022-04-26 09:28:07 +0500

    just uploaded the practice project

---
+++ app/src/main/java/com/bsef19m517/mc01/MainActivity.java
@@ -0,0 +1,26 @@
+package com.bsef19m517.mc01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etName;
+    EditText etPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etName = (EditText) findViewById(R.id.editTextTextPersonName);
+        etPass = (EditText) findViewById(R.id.editTextTextPersonName2);
+    }
+
+    public void Function1(View view) {
+       String myName =  etName.getText().toString();
+       String myPass = etPass.getText().toString();
+        Toast.makeText(MainActivity.this, "Welcome " + myName + "\nPassword: " + myPass , Toast.LENGTH_LONG).show();
+    }
+}
