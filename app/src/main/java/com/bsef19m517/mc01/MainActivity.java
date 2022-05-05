b8027a66924194bd8ac893eec321a3bcff9b8265
BSEF19M517 <bsef19m517@pucit.edu.pk>
2022-05-05 20:07:58 +0500

    Activity LifeCycle A

--- app/src/main/java/com/bsef19m517/mc01/MainActivity.java
+++ app/src/main/java/com/bsef19m517/mc01/MainActivity.java
@@ -2,25 +2,51 @@ package com.bsef19m517.mc01;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText etName;
-    EditText etPass;
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_calc);
-        etName = (EditText) findViewById(R.id.editTextTextPersonName);
-        etPass = (EditText) findViewById(R.id.editTextTextPersonName2);
+        Log.d(TAG,":::onCreate function:::");
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG,":::onStart function:::");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG,":::onResume function:::");
     }
 
-    public void Function1(View view) {
-       String myName =  etName.getText().toString();
-       String myPass = etPass.getText().toString();
-        Toast.makeText(MainActivity.this, "Welcome " + myName + "\nPassword: " + myPass , Toast.LENGTH_LONG).show();
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,":::onPause function:::");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG,":::onRestart function:::");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG,":::onStop function:::");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,":::onDestroy function:::");
     }
 }
