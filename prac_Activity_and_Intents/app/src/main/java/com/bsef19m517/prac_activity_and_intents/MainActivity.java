8154f6a4a459a114450864f23c72304a8974af4b
BSEF19M517 <bsef19m517@pucit.edu.pk>
2022-05-21 19:04:37 +0500

    re-uploading

---
+++ prac_Activity_and_Intents/app/src/main/java/com/bsef19m517/prac_activity_and_intents/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bsef19m517.prac_activity_and_intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
